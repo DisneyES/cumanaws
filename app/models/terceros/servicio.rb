ef414c3c3308b76329eb6180597046c1373b29bf
Javier E. <jaedns@gmail.com>
2015-04-23 17:16:31 -0430

    Manage third party services.

---
+++ app/models/terceros/servicio.rb
@@ -0,0 +1,44 @@
+# Manage third party services
+class Terceros::Servicio
+  
+  # To initialize the class
+  def initialize
+    
+  end
+  
+  # To start a service
+  def self.iniciar(nombre)
+    system('systemctl','start',nombre+'.service')
+  end
+  
+  # To stop a service
+  def self.detener(nombre)
+    system('systemctl','stop',nombre+'.service')
+  end
+  
+  # To restart a service
+  def self.reiniciar(nombre)
+    system('systemctl','restart '+nombre+'.service')
+  end
+  
+  # To reload a service
+  def self.recargar(nombre)
+    system('systemctl','reload '+nombre+'.service')
+  end
+  
+  # To get the service status
+  def self.estado(nombre)
+    system('systemctl','-l','status',nombre+'.service')
+  end
+  
+  # To execute other param of the service
+  def self.ejecutar(nombre,param)
+    system('systemctl',param,nombre+'.service')
+  end
+  
+  # To get a list of the services
+  def self.lista
+    system('systemctl','-t','service','-a')
+  end
+  
+end
