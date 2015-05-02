e6a94b8bea03e7005c49e2ec5329da62dbd1be6f
Javier E. <jaedns@gmail.com>
2015-05-02 15:57:01 -0430

    To set the languaje, country and currency.

---
+++ app/controllers/locales_controller.rb
@@ -0,0 +1,27 @@
+class LocalesController < ApplicationController
+  
+  def idioma
+    if params[:idioma]
+      
+    else
+      
+    end
+  end
+  
+  def pais
+    if params[:pais]
+      
+    else
+      
+    end
+  end
+  
+  def moneda
+    if params[:moneda]
+      
+    else
+      
+    end
+  end
+  
+end
