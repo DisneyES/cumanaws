8744c7e52ff1247123476677bca8d0f8a001c17a
Javier E. <jaedns@gmail.com>
2015-05-02 16:01:10 -0430

    the user must specified the language and the framework of the web application.

--- app/models/aplicacion_web.rb
+++ app/models/aplicacion_web.rb
@@ -2,6 +2,8 @@ class AplicacionWeb
   include Mongoid::CumanawsBase
   
   field :nombre, type: String
+  field :lenguaje, type: String # nodejs, php, python, ruby
+  field :framework, type: String
   field :app_server, type: String # php-fpm, unicorn, gunicorn, nodejs
   field :dir, type: String
   field :entorno, type: String # production, development, text, etc...
